43d117e09e5ebc4ad872d62608dee8ecc3ea8e49
dontbesoshy <kajdaspawel@gmail.com>
2022-09-25 18:03:23 +0200

    second commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '70f3e0074de41652a4c14ac6a5d6498ef15eab5e',
+        'name' => 'user/my_app',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'user/my_app' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '70f3e0074de41652a4c14ac6a5d6498ef15eab5e',
+            'dev_requirement' => false,
+        ),
+    ),
+);
